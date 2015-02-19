6faaa0995dbbdda83d782b845830c6c48e15b09f
Andrew Clinton <ddrew.clinton@gmail.com>
2015-02-18 19:37:23 -0500

    calculator should be fully functional now. even the extra buttons. still having issues w/ floating point math though

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var source;
 
+/* ==== HELPER METHODS TO EVAL EXPRESSION ==== */
 function valueOfDigit() {
     var value = source.charAt(0);
     source = source.slice(1, source.length);
@@ -10,7 +11,7 @@ function valueOfDigitSeq() {
     var value = parseFloat(valueOfDigit());
     while (isDigit(source.charAt(0))) {
         value *= 10;
-        value += valueOfDigit();
+        value += parseFloat(valueOfDigit());
     }
     return value;
 }
@@ -60,10 +61,76 @@ function isDigit(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-function main() {
-    source = document.getElementById('expression').value;
+function calculate() {
+    var expression = document.getElementById('expression');
+    source = expression.value;
     var value = valueOfExpr();
-    console.log(value);
+    expression.value = value.toString();
 }
 
-main();
+/* ==== CALCULATOR BUTTON CONTROLS ==== */
+function termCapture(self) {
+    var term = self.value;
+    var expression = document.getElementById('expression');
+    var newExp = expression.value + term;
+    expression.value = newExp;
+}
+
+function clearOne(self) {
+    var expression = document.getElementById('expression');
+    var oldExp = expression.value;
+    var newExp = oldExp.slice(0, oldExp.length-1);
+    expression.value = newExp;
+}
+
+function AC(self) {
+    document.getElementById('expression').value = '';
+}
+
+function memAdd(self) {
+    var memory = localStorage.getItem('calc_memory') || 0;
+    memory = parseFloat(memory);
+    var value = document.getElementById('expression').value;
+    if(isDigit(value)) {
+        value = parseFloat(value);
+        memory += value;
+        localStorage.setItem('calc_memory', memory);
+        message("Added " + value + " to memory.");
+    }
+}
+
+function memSub(self) {
+    var memory = localStorage.getItem('calc_memory') || 0;
+    memory = parseFloat(memory);
+    var value = document.getElementById('expression').value;
+    if(isDigit(value)) {
+        value = parseFloat(value);
+        memory -= value;
+        localStorage.setItem('calc_memory', memory);
+        message("Subtracted " + value + " from memory.");
+    }
+}
+
+function memClear(self) {
+    localStorage.setItem('calc_memory', 0);
+    message("Cleared the memory.");
+}
+
+function memRecall(self) {
+    var expression = document.getElementById('expression');
+    var memory = localStorage.getItem('calc_memory') || 0;
+    expression.value = expression.value + memory;
+}
+
+/* ==== MESSAGE METHODS ==== */
+function message(msg) {
+    var el = document.getElementById('message');
+    el.style.opacity = "0.0";
+    setTimeout(function () {
+        el.innerHTML = msg;
+        el.style.opacity = "1.0";
+        setTimeout(function () {
+            el.style.opacity = "0.0";
+        },2000);
+    }, 500);
+}
